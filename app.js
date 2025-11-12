e163c7cf700f449f68db4e43599038d9f4ba4d44
Dylan Simon <grvdylans@gmail.com>
2025-11-12 13:10:47 -0700

    Homepage now pulls from 'views'

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const assetsPath = path.join(__dirname, "public");
 
 // my server
 const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.render("index", { message: "Welcome!" });
 });
 
 const PORT = 3000;
@@ -14,5 +21,8 @@ app.listen(PORT, (error) => {
         throw error;
     }
 
-    console.log("server running");
+    console.log("\nserver running");
+    console.log("file: " + __filename);
+    console.log("directory: " + __dirname);
+    console.log("public assets: " + assetsPath);
 });
