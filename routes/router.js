d78f94d14c7cf33a89657713d7a503bdb03120d3
Dylan Simon <grvdylans@gmail.com>
2025-11-12 13:43:34 -0700

    Complete step 4/12

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,31 @@
 import { Router } from "express";
 
 const router = Router();
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+    {
+        text: "I <3 Omarchy!",
+        user: "Pewdiepie",
+        added: new Date(),
+    },
+    {
+        text: "DHH MUST BE STOPPED",
+        user: "Random Guy on the Internet",
+        added: new Date(),
+    },
+];
 
 router.get("/", (req, res) => {
-    res.render("index", { message: "Welcome!" });
+    res.render("index", { message: "Welcome!", messages: messages });
 });
 
 export default router;
